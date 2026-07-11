e7f0efa8381344fe0d1c79766917da9f88fa2df8
questc0der <pour64moi@gmail.com>
2026-07-12 02:39:22 +0300

    done price report

--- Module-01/codeops-portfolio/day03/practice.py
+++ Module-01/codeops-portfolio/day03/practice.py
@@ -4,3 +4,8 @@ unique = {city for city in cities}
 count = {city: cities.count(city) for city in unique}
 print(count)
 print(unique)
+
+#Price Report
+groceries = {"banana": 25, "Avocado": 30, "Tomato": 45, "Orange": 30, "apple": 150}
+for grocery in groceries:
+    print(f"{grocery}: {groceries[grocery]}")
