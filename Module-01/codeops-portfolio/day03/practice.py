6c9f9cf93df48878b1af4ef91a04c3ab7695f3c1
questc0der <pour64moi@gmail.com>
2026-07-12 02:24:35 +0300

    add city count

--- Module-01/codeops-portfolio/day03/practice.py
+++ Module-01/codeops-portfolio/day03/practice.py
@@ -1,4 +1,6 @@
 #Unique cities
 cities = ["Addis Ababa", "Adama","Bahir Dar","Mekelle","Hawassa","Addis Ababa","Jimma","Bahir Dar","Dire Dawa","Hawassa"]
 unique = {city for city in cities}
+count = {city: cities.count(city) for city in unique}
+print(count)
 print(unique)
