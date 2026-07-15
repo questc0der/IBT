2120d479977bed677d83d14e7a0e1b5ec3bd06c8
questc0der <pour64moi@gmail.com>
2026-07-15 23:25:39 +0300

    class and subclass created

---
+++ Module-01/codeops-portfolio/day05/practice.py
@@ -0,0 +1,12 @@
+class Vehicle:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+    def describe():
+        pass
+
+class Car(Vehicle):
+    pass
+
+class Truck(Vehicle):
+    pass
