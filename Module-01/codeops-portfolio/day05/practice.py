4fece6e09ffa4e8cc0e38234f56b5a52064eccc2
questc0der <pour64moi@gmail.com>
2026-07-16 00:05:45 +0300

    done implementing abstraction

--- Module-01/codeops-portfolio/day05/practice.py
+++ Module-01/codeops-portfolio/day05/practice.py
@@ -1,12 +1,30 @@
-class Vehicle:
+from abc import ABC, abstractmethod
+class Vehicle(ABC):
     def __init__(self, make, model):
         self.make = make
         self.model = model
-    def describe():
-        pass
+    def describe(self):
+        return f"Make: {self.make}, Model: {self.model}"
+    
+    @abstractmethod
+    def wheels(self): ...
 
 class Car(Vehicle):
-    pass
+    def wheels(self):
+        return 4
 
 class Truck(Vehicle):
-    pass
+    def __init__(self, make, model, capacity):
+        super().__init__(make, model)
+        self.capacity = capacity
+    
+    def describe(self):
+        return f"{super().describe()}, Capacity: {self.capacity}"
+
+    def wheels(self):
+        return 9
+
+vehicles = [Truck('Volvo', 'VV-33', 20),Car('Toyota', 'Corolla'), Truck('ISUZU', 'IS-33', 10), Car('Volkswagen', 'VW-115')]
+
+for vehicle in vehicles:
+    print(vehicle.describe())
