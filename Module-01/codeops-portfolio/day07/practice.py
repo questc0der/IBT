c5de48f6e6e2c83e26d42632403a985b92d62430
questc0der <pour64moi@gmail.com>
2026-07-19 18:00:54 +0300

    time complexity for DS

---
+++ Module-01/codeops-portfolio/day07/practice.py
@@ -0,0 +1,5 @@
+# list index .... O(1) because item is accessed with index and the index let's us know the position directly
+# single loop ... O(n) because it has to iterate each value of the list
+#nested loop... O(n^2) because one loop runs n times, and inside it another loop runs n times.
+#dict lookup ... O(1) because items are accessed with the key value it will directly access that key's value
+#binary search ... O(log n) binary search be dividing the list in two then drops the other one it will be log n (shortly it halfs the list)
